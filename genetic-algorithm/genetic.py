1232d6be58f8da2226d0f1ff74998bf34a3c8d9b
Eliot Ball <eb@eliotball.com>
2015-05-06 16:04:36 +0100

    Add report

--- genetic-algorithm/genetic.py
+++ genetic-algorithm/genetic.py
@@ -1,6 +1,7 @@
 import random
 import operator
 import time
+import json
 
 def random_bit():
     return random.random() > 0.5
@@ -22,21 +23,76 @@ class Population():
         self.members += [[True for i in xrange(string_length)]]
         self.members += [[False for i in xrange(string_length)]]
 
-    def improve(self, scoring_function, mutation_probability=0.01):
+    def members_to_string(self, members):
+        member_strings = []
+        for member in members:
+            member_strings += [""]
+            for bit in member:
+                if bit == True:
+                    member_strings[-1] += "1"
+                else:
+                    member_strings[-1] += "0"
+        member_strings.sort()
+        return " ".join(member_strings)
+
+    def __repr__(self):
+        return self.members_to_string(self.members)
+
+    def get_members_from_string(self, string):
+        members = []
+        for member_string in string.split(" "):
+            members += [[]]
+            for bit in member_string:
+                if bit == "0":
+                    members[-1] += [False]
+                elif bit == "1":
+                    members[-1] += [True]
+        return members
+
+    def get_best_few(self, scoring_function):
         scores = []
         for member in self.members:
-            scores += [(member, scoring_function(member))]
-        total_fitness = sum([score[1] for score in scores])
-        scores.sort(key=operator.itemgetter(1))  # smaller is better
-        best_few = scores[:self.select_best]
+            score, other_data = scoring_function(member)
+            scores += [{
+                "member": member,
+                "score": score,
+                "other_data": other_data
+            }]
+            scores.sort(key=lambda s: s["score"])  # smaller is better
+        total_fitness = sum([score["score"] for score in scores])
+        return total_fitness, [i["member"] for i in scores[:self.select_best]], scores
+
+    def improve(self, scoring_function, mutation_probability=0.01,
+            recording=False, log_file=None):
+        total_fitness, best_few, all_scores = self.get_best_few(scoring_function)
+        if recording:
+            log_line = {}
+            log_line["members"] = str(self)
+            log_line["best_few"] = self.members_to_string(best_few)
+            for score in all_scores:
+                score["member"] = self.members_to_string([score["member"]])
+            log_line["all_scores"] = all_scores
+            log_file.write(json.dumps(log_line) + "\n")
         self.generate_new_members(best_few, mutation_probability)
         return total_fitness
 
+    def audit(self, scoring_function, log_file):
+        while True:
+            next_line = log_file.readline()
+            if next_line.strip() == "":
+                break
+            self.members = self.get_members_from_string(next_line)
+            _, best_few = self.get_best_few(scoring_function)
+            now_best_set = set(self.members_to_string(best_few).split(" "))
+            earlier_best_set = set(log_file.readline().split(" "))
+            diff = len(now_best_set ^ earlier_best_set)
+            print "Difference from earlier run: %s" % diff
+
     def generate_new_members(self, best_few, mutation_probability=0.01):
-        self.members = [i[0] for i in best_few]
+        self.members = best_few
         for member_count in xrange(len(best_few), self.size):
-            left_parent = random.choice(best_few)[0]
-            right_parent = random.choice(best_few)[0]
+            left_parent = random.choice(best_few)
+            right_parent = random.choice(best_few)
             # Crossover
             assert len(left_parent) == len(right_parent)
             crossover_point = random.randint(0, len(left_parent))
@@ -48,11 +104,14 @@ class Population():
                     new_member[i] = not new_member[i]
             self.members += [new_member]
 
-    def get_good_example(self, scoring_function, threshold=0.001):
+    def get_good_example(self, scoring_function, threshold=0.001,
+            recording=False, log_file=None):
         start_time = time.time()
-        last_fitness = self.improve(scoring_function)
+        last_fitness = self.improve(scoring_function, recording=recording,
+                log_file=log_file)
         print "Iteration 1    Pop fitness: " + str(last_fitness)
-        this_fitness = self.improve(scoring_function)
+        this_fitness = self.improve(scoring_function, recording=recording,
+                log_file=log_file)
         print "Iteration 2    Pop fitness: " + str(this_fitness)
         iterations = 2
         converged_for = 0
@@ -60,7 +119,8 @@ class Population():
         while converged_for < 3:
             last_fitness = this_fitness
             mutation_probability *= 0.95  # decay mutations for tuning
-            this_fitness = self.improve(scoring_function, mutation_probability)
+            this_fitness = self.improve(scoring_function, mutation_probability,
+                    recording=recording, log_file=log_file)
             if 1 - threshold < float(this_fitness) / last_fitness < 1 + threshold:
                 converged_for += 1
             else:
@@ -69,6 +129,8 @@ class Population():
             print "Iteration " + str(iterations) + "    Pop fitness: " \
                     + str(this_fitness) + "   Converging for " + str(converged_for) \
                     + "   Time: " + str(round(time.time() - start_time, 2)) + "s"
+        if recording:
+            log_file.write(json.dumps({"final_population": str(self)}))
         return self.members[0]
 
 p = Population()
--- genetic-algorithm/run.py
+++ genetic-algorithm/run.py
@@ -1,5 +1,6 @@
 import genetic
 import replace
+import sys
 
 import os
 import math
@@ -12,19 +13,47 @@ def is_int(candidate):
         return False
 
 def get_subclauses_from_file(file_name):
-    os.system("mkfifo clauses.fifo lines.fifo")
-    os.system("cat \"" + file_name + "\" | perl -pe 's/ 0 *$//g' | tail -n +2 > lines.fifo &")
-    os.system("./apriori -m2 -n3 -s-30 lines.fifo clauses.fifo &")
+    os.system("mkfifo clauses-%s.fifo lines-%s.fifo" % (file_name, file_name)
+    os.system("cat \"" + file_name + "\" | perl -pe 's/ 0 *$//g' | tail -n +2 > lines-%s.fifo &" % file_name)
+    os.system("./apriori -m2 -n3 -s-30 lines%s.fifo clauses-%s.fifo &" % (file_name, file_name))
     subclauses = []
-    with open("clauses.fifo", "r") as fifo:
+    with open("clauses-%s.fifo" % file_name, "r") as fifo:
         for line in fifo:
             subclauses += [[int(literal) for literal in line.split(" ") if is_int(literal)]]
-    os.system("rm clauses.fifo lines.fifo")
+    os.system("rm clauses-%s.fifo lines-%s.fifo" % (file_name, file_name))
     return subclauses
 
 def get_formula_from_file(file_name):
     with open(file_name) as cnf_file:
         return replace.Formula(cnf_file.read())
+
+
+class SolverResult:
+    def __init__(self, output):
+        self.output = output
+        self.restarts = self.get_stat("restarts", "\n", int)
+        self.conflicts = self.get_stat("conflicts", "(", int)
+        self.decisions = self.get_stat("decisions", "(", int)
+        self.propagations = self.get_stat("propagations", "(", int)
+        self.conflict_literals = self.get_stat("conflict literals", "(", int)
+        self.time = self.get_stat("CPU time", "s", float)
+
+    def get_stat(self, name, end_marker, finish=lambda x: x):
+        return finish(self.output.split(name)[1].split(": ")[1].split(end_marker)[0].strip())
+    
+    def get_bundle(self):
+        return {
+            "restarts": self.restarts,
+            "conflics": self.conflicts,
+            "decisions": self.decisions,
+            "propagations": self.propagations,
+            "conflict_literals": self.conflict_literals,
+            "time": self.time
+        }
+    
+    def __lt__(self, other):
+        return self.time < other.time
+
 
 def run_solver_with_replacements(cnf, replacements):
     new_formula = cnf.clone()
@@ -34,10 +63,7 @@ def run_solver_with_replacements(cnf, replacements):
         cnf.write(new_formula.to_cnf_file())
     output = os.popen("./minisat replaced.cnf").read()
     os.system("rm replaced.cnf")
-    return output
-
-def get_time_from_output(output):
-    return float(output.split("CPU time")[1].split(": ")[1].split(" s")[0])
+    return SolverResult(output)
 
 def get_subclauses_from_mask(subclauses, mask):
     used_subclauses = []
@@ -46,30 +72,23 @@ def get_subclauses_from_mask(subclauses, mask):
             used_subclauses += [subclauses[i]]
     return used_subclauses
 
-def run_genetic_algorithm(formula, subclauses):
+def run_genetic_algorithm(formula, subclauses, log_name):
     threshold = 0.005
     print "INITIAL RUN"
-    with open("log.txt", "w") as log_file:
+    with open(log_name, "w") as log_file:
         population = genetic.Population(string_length=len(subclauses),
-                size=500, select_best=25)  # should be 5000, 50
+                size=50, select_best=25)  # should be 5000, 50
         def scoring_function(mask):
             used_subclauses = get_subclauses_from_mask(subclauses, mask)
-            return time_accurately(formula, used_subclauses)
+            result = run_solver_with_replacements(formula, used_subclauses)
+            return result.time, result.get_bundle()
         good_example = population.get_good_example(scoring_function,
                 threshold=threshold, recording=True, log_file=log_file)
     print "CHECKING IF RESULT IS MAINTAINED"
-    with open("log.txt", "r") as log_file:
+    with open(log_name, "r") as log_file:
         # Check that the run is the same next time
         population.audit(scoring_function, log_file);
     return get_subclauses_from_mask(subclauses, good_example)
-
-def time_accurately(formula, subclauses):
-    repeats = 10
-    total = 0.0
-    for i in xrange(repeats):
-        total += get_time_from_output(
-                run_solver_with_replacements(formula, subclauses))
-    return total / repeats
 
 def get_time_distribution(formula, repeats=10):
     times = []
@@ -91,10 +110,13 @@ def get_time_distribution(formula, repeats=10):
     print "Std dev: %6f (%6f)" % (std_dev, std_dev / mean)
 
 if __name__ == "__main__":
-    subclauses = get_subclauses_from_file("goldb-heqc-term1mul.cnf")
+    formula_file = sys.argv[1]
+    log_name = sys.argv[2]
+    subclauses = get_subclauses_from_file(formula_file)
+    print "RUN FOR %s" % formula_file
     print str(len(subclauses)) + " repeated subclauses found"
-    formula = get_formula_from_file("goldb-heqc-term1mul.cnf")
-    result = run_genetic_algorithm(formula, subclauses)
+    formula = get_formula_from_file(formula_file)
+    result = run_genetic_algorithm(formula, subclauses, log_name=log_name)
     print str(len(result)) + " of " + str(len(subclauses)) + " selected by genetic algorithm"
     print result
     before_time = time_accurately(formula, [])
